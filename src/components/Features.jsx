a4791fd04d67213fa29e2d615913ffe567684bdf
Ritin George <ritingeorge.d@gmail.com>
2022-09-06 20:12:37 +0530

    bug-fix

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -6,7 +6,7 @@ function Features() {
     <div className='w-full min-h-[100vh] align-middle flex flex-col items-center text-white justify-center bg-[#ef9a9a]'>
         <div className='flex items-center font-bold text-md md:text-xl lg:text-4xl'>
             <div>DELIVERED TO YOU</div> 
-            <img className='-mx-[3.3em] pl-3 z-[10] w-[250px] md:w-[300px] lg:w-[450px]' src={tShirt} alt="t-shirt" />
+            <img className='pl-3 z-[10] w-[250px] md:w-[300px] lg:w-[450px]' src={tShirt} alt="t-shirt" />
             <div>STRAIGHT FROM EARTH</div>
         </div>
         <p className='text-center mt-10 max-w-[500px]'>
